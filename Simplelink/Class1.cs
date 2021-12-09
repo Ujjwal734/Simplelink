facb49ce50c9428202397704fd9eaa92e6c6d087
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2021-12-09 06:09:08 +0530

    linked list

---
+++ Simplelink/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplelink
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int i)
+        {
+            data = i;
+            next = null;
+        }
+        public void Display()
+        {
+            Console.Write(" "+ data);
+            if (next != null)
+            {
+                next.Display();
+            }
+        }
+    }
+ /*   class programme
+    {
+        static void Main(string[] args)
+        {
+            Node mynode = new Node(56);
+            mynode.next = new Node(30);
+            mynode.next.next = new Node(70);
+            mynode.Display();
+        }
+    }*/
+}
--- Simplelink/Program.cs
+++ Simplelink/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to linkedlist programme:- ");
+//Console.WriteLine("Welcome to linkedlist programme:- ");
+using Simplelink;
+Node mynode = new Node(56);
+mynode.next = new Node(30);
+mynode.next.next = new Node(70);
+mynode.Display();
